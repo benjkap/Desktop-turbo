05a4bd51dba9944bbad4a3e14489efc40e1cc166
JCBouville <jean-charles.bouville@isen.yncrea.fr>
2021-01-06 16:26:32 +0100

    Clock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import {HomeComponent} from './home/home.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthService} from './service/auth/auth.service';
 import {RegisterComponent} from './register/register.component';
+import { ClockComponent } from './clock/clock.component';
+import { DatePipe } from '@angular/common'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    ClockComponent
   ],
   imports: [
     FormsModule,
@@ -24,7 +27,7 @@ import {RegisterComponent} from './register/register.component';
     HttpModule,
     ReactiveFormsModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule {
