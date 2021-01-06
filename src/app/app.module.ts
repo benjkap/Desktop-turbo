e01f96bd4ebdbe48e66959d6dcf2f3ed9e05901b
Marco <marco.escribano@yncrea.fr>
2021-01-06 15:10:06 +0100

    ajout agenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule , FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ScheduleAllModule, RecurrenceEditorAllModule } from '@syncfusion/ej2-angular-schedule';
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
@@ -9,23 +10,28 @@ import {HomeComponent} from './home/home.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthService} from './service/auth/auth.service';
 import {RegisterComponent} from './register/register.component';
+import { AgendaComponent } from './widgets/agenda/agenda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    AgendaComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ScheduleAllModule,
+     RecurrenceEditorAllModule 
   ],
   providers: [AuthService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+ // schémas : [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule {
 }
