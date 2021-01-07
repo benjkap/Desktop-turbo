99b169b47702f7074ea09e4da53673a8e9b4666d
py2378 <colasbou@gmail.com>
2021-01-07 10:44:23 +0100

    Revert "Merge branch 'profil+checklist' into global"
    This reverts commit 587f2d6d21971ecec7d5fdf54f8d7ed6d47423e5.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,6 @@ import {RegisterComponent} from './register/register.component';
 import { ClockComponent } from './widget/clock/clock.component';
 import { DatePipe } from '@angular/common';
 import { CalculatorComponent } from './widget/calculator/calculator.component';
-import { ProfilComponent } from './profil/profil.component';
-import { CheckListComponent } from './check-list/check-list.component';
-import { RepertoireComponent } from './repertoire/repertoire.component';
 
 @NgModule({
   declarations: [
@@ -24,9 +21,6 @@ import { RepertoireComponent } from './repertoire/repertoire.component';
     RegisterComponent,
     ClockComponent,
     CalculatorComponent
-    ProfilComponent,
-    CheckListComponent,
-    RepertoireComponent
   ],
   imports: [
     FormsModule,
