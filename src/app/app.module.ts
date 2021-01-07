1831180fe83907ecc24205f84af379ec9ebb648b
Marco <marco.escribano@yncrea.fr>
2021-01-07 17:54:39 +0100

    agenda et bloc note

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import '../polyfills';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule , FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ScheduleAllModule, RecurrenceEditorAllModule } from '@syncfusion/ej2-angular-schedule';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {DragDropComponent} from './drag-drop/drag-drop.component';
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
@@ -11,6 +15,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {AuthService} from './service/auth/auth.service';
 import {RegisterComponent} from './register/register.component';
 import { AgendaComponent } from './widgets/agenda/agenda.component';
+import { NoteComponent } from './widgets/note/note.component';
+import { SnoteComponent } from './widgets/note/note/snote.component';
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,10 @@ import { AgendaComponent } from './widgets/agenda/agenda.component';
     LoginComponent,
     HomeComponent,
     RegisterComponent,
-    AgendaComponent
+    AgendaComponent, 
+    NoteComponent, 
+    SnoteComponent, 
+    DragDropComponent,
   ],
   imports: [
     FormsModule,
@@ -27,11 +38,16 @@ import { AgendaComponent } from './widgets/agenda/agenda.component';
     HttpModule,
     ReactiveFormsModule,
     ScheduleAllModule,
-     RecurrenceEditorAllModule 
+    RecurrenceEditorAllModule,
+    DragDropModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [AuthService],
+
   bootstrap: [AppComponent],
- // schémas : [ CUSTOM_ELEMENTS_SCHEMA ]
+  entryComponents: [DragDropComponent]
+
 })
 export class AppModule {
 }
