6ea97d7df6f83c6bd54318a88b5e05fe2efb762e
Benjamin <benjamindelattre03@gmail.com>
2021-01-08 10:39:20 +0100

    admin Panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
 import { ProfilComponent } from './profil/profil.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 import {AuthGuard} from './service/auth/auth.guard';
 
@@ -23,6 +24,11 @@ const routes: Routes = [
     path: 'profil',
     canActivate: [AuthGuard],
     component: ProfilComponent
+  },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    component: AdminPanelComponent
   },
   {
     path: 'register',
