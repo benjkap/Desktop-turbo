22be8e621311339f074b32c41ed1773a17fe353e
Marco <marco.escribano@yncrea.fr>
2021-01-08 08:52:10 +0100

    z-index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,60 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'desktop-turbo';
+  statusClass1 = 'not-active';
+
+  setActiveClass1(){
+    if(this.statusClass1 == 'not-active'){
+      this.statusClass1 = 'active';
+      this.statusClass2 = 'not-active';
+      this.statusClass3 = 'not-active';
+      this.statusClass4 = 'not-active';
+    }
+    else{
+      this.statusClass1 = 'not-active';
+    }
+    
+  }
+  statusClass2 = 'not-active';
+
+  setActiveClass2(){
+    if(this.statusClass2 == 'not-active'){
+      this.statusClass2 = 'active';
+      this.statusClass1 = 'not-active';
+      this.statusClass3 = 'not-active';
+      this.statusClass4 = 'not-active';
+    }
+    else{
+      this.statusClass2 = 'not-active';
+    }
+    
+  }
+
+  statusClass3 = 'not-active';
+
+  setActiveClass3(){
+    if(this.statusClass3 == 'not-active'){
+      this.statusClass3 = 'active';
+      this.statusClass4 = 'not-active';
+      this.statusClass2 = 'not-active';
+    }
+    else{
+      this.statusClass3 = 'not-active';
+    }
+    
+  }
+  statusClass4 = 'not-active';
+
+  setActiveClass4(){
+    if(this.statusClass4 == 'not-active'){
+      this.statusClass4 = 'active';
+      this.statusClass3 = 'not-active';
+      this.statusClass2 = 'not-active';
+      this.statusClass1 = 'not-active';
+    }
+    else{
+      this.statusClass4 = 'not-active';
+    }
+    
+  }
 }
